8b0f638aa7e7a66f0afdd89137c59727698b3710
CasperGuldbechNielsen <casp674d@edu.easj.dk>
2016-02-03 10:12:57 +0100

    Finalized the project

--- Basic Serialization - Console/Car.cs
+++ Basic Serialization - Console/Car.cs
@@ -7,11 +7,13 @@ namespace Basic_Serialization___Console
     {
         public string Model { get; set; } 
         public string RegNo { get; set; }
+        public Owner Owner { get; set; }
 
-        public Car(string model, string regNo)
+        public Car(string model, string regNo, Owner owner)
         {
             this.Model = model;
             this.RegNo = regNo;
+            this.Owner = owner;
         }
     }
 }
--- Basic Serialization - Console/Program.cs
+++ Basic Serialization - Console/Program.cs
@@ -13,21 +13,99 @@ namespace Basic_Serialization___Console
     {
         static void Main(string[] args)
         {
-            Owner owner = new Owner(1, "Casper");
+            List<Owner> owners = new List<Owner>();
+
+            owners.Add(new Owner(1, "Casper"));
+            owners.Add(new Owner(2, "Istvan"));
+            owners.Add(new Owner(3, "Bianca"));
+
+            List<Car> cars = new List<Car>();
+
+            cars.Add(new Car("Volvo", "AA 12 123", owners[0]));
+            cars.Add(new Car("Bmw", "BB 98 987", owners[1]));
+            cars.Add(new Car("Corsa", "HJ 53 348", owners[2]));
+
+            List<object> persistence = new List<object>();
+
+            persistence.Add(owners);
+            persistence.Add(cars);
 
             IFormatter formatter = new BinaryFormatter();
-            Stream stream = new FileStream("Owner.bin", FileMode.Create, FileAccess.Write, FileShare.None);
-            formatter.Serialize(stream, owner);
-            stream.Close();
+            Stream stream = null;
+            Stream outputStream = null;
 
-            Stream outputStream = new FileStream("Owner.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
-            Owner outputOwner = (Owner)formatter.Deserialize(outputStream);
-            stream.Close();
+            List<object> outputOwner;
 
-            string message = outputOwner.Id + ", " + outputOwner.Name;
+            try
+            {
+                stream = new FileStream("Owner.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+                formatter.Serialize(stream, persistence);
+            }
+            catch (SerializationException sie)
+            {
+                throw sie;
+            }
+            catch (IOException e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (stream != null)
+                {
+                    stream.Close();
+                }
+            }
 
-            Console.WriteLine(message);
+            try
+            {
+                outputStream = new FileStream("Owner.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
+                outputOwner = (List<object>) formatter.Deserialize(outputStream);
+            }
+            catch (SerializationException sie)
+            {
+                throw sie;
+            }
+            catch (IOException e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (outputStream != null)
+                {
+                    outputStream.Close();
+                }
+            }
             
+
+            List<Owner> newOwners = new List<Owner>();
+            List<Car> newCars = new List<Car>();
+
+            newOwners = (List<Owner>)outputOwner[0];
+            newCars = (List<Car>)outputOwner[1];
+
+            foreach (var item in newOwners)
+            {
+                string message = item.Id + ", " + item.Name;
+                Console.WriteLine(message);
+            }
+
+            Console.WriteLine("==================");
+
+            foreach (var item in newCars)
+            {
+                string message = item.Model + ", " + item.RegNo + ", " + item.Owner.Name;
+                Console.WriteLine(message);
+            }
+
+            Console.WriteLine("==================");
+
+            if (newOwners[0].Equals(newCars[0].Owner))
+            {
+                Console.Write("It is!");
+            }
+
             Console.ReadKey();
         }
     }
